27c02b1368b0f9150371f2060c94c1aad7def143
vansh <vanshsin784@gmail.com>
2024-05-11 19:32:53 +0530

    ADD 2 more algorithm of deletion

--- c++/Linkedlist/deletionINLL.cpp
+++ c++/Linkedlist/deletionINLL.cpp
@@ -4,6 +4,7 @@
     --- value
     --- last
 */
+// All these deletion algorithm are of the same time complexity is O(N) and the space complexity is O(1).
 #include<bits/stdc++.h>
 using namespace std;
 class Node{
@@ -45,11 +46,61 @@ Node* deletetail(Node* head){
     temp->next=nullptr;
     return head;
 }
+Node* deletek(Node* head,int k){
+    // edge case -- if the head=null or linked list has only 1 node
+    if(k==1){
+        // we have to delete the head
+        Node* temp=head;
+        head=head->next;
+        delete temp;
+    }
+    int cnt=0;
+    Node* temp=head;
+    Node* p=NULL;
+    while(temp!=NULL){
+        cnt++;
+        if(cnt==k){
+            p->next=p->next->next;
+            free(temp);
+            break;
 
+        }
+        p=temp;
+        temp=temp->next;
+    }
+    return head;
+
+}
+Node* removeval(Node* head, int k){
+    // edge case
+    if(head==NULL) return head;
+    if(head->data==k){
+        Node*temp=head;
+        head=head->next;
+        free(temp);
+        return head;
+    }
+    Node* temp=head;
+    Node* p=NULL;
+    while(temp!=NULL){
+        if(temp->data==k){
+            p->next=p->next->next;
+            free(temp);
+            break;
+        }
+        p=temp;
+        temp=temp->next;
+    }
+    return head;
+
+}
 int main(){
     vector<int>arr={2,4,6,8,10};
+    int k;
+    cout<<"Enter the Value  ";
+    cin>>k;
     Node* head=cnvrtarr2LL(arr);
-    head=deletetail(head);
+    head=removeval(head,k);
     Node* temp=head;
     while(temp!=NULL){
         cout<<temp->data<<" ";
